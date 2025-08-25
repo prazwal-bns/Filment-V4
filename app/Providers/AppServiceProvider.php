46aae72355922bc87901f78f276b1351ad79b10f
prazwal-bns <prajwalbns15@gmail.com>
2025-08-25 12:35:50 +0545

    enhance(AppServiceProvider): Configure model settings and Filament panel, and apply label formatting for TextColumn

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +23,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->configureModels();
+        $this->configureFilament();
+
+        TextColumn::configureUsing(function (TextColumn $column): void {
+            $column->label(Str::headline($column->getLabel()));
+        });
+    }
+
+    /**
+     * Configure the models for the application.
+     */
+    private function configureModels(): void
+    {
         Model::preventLazyLoading(! App::isProduction());
+    }
+
+    /**
+     * Configure the Filament panel for the application.
+     */
+    private function configureFilament(): void
+    {
         \Filament\Resources\Pages\CreateRecord::disableCreateAnother();
     }
 }
