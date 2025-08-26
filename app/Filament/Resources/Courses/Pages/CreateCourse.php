75a9efe2c8768098955370c629b457b0c3c9efd6
prazwal-bns <prajwalbns15@gmail.com>
2025-08-26 09:38:29 +0545

    enhance(CreateCourse): Add custom label 'Submit' to the create form action

--- app/Filament/Resources/Courses/Pages/CreateCourse.php
+++ app/Filament/Resources/Courses/Pages/CreateCourse.php
@@ -3,9 +3,16 @@
 namespace App\Filament\Resources\Courses\Pages;
 
 use App\Filament\Resources\Courses\CourseResource;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCourse extends CreateRecord
 {
     protected static string $resource = CourseResource::class;
+
+    protected function getCreateFormAction(): Action
+    {
+        return parent::getCreateFormAction()
+            ->label('Submit');
+    }
 }
