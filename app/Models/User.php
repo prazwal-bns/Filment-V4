cf960ed17dfbc6f5007953173a25f148a0dcac26
prazwal-bns <prajwalbns15@gmail.com>
2025-08-26 09:39:08 +0545

    add(User): Implement method to check user access to Filament admin panel and create Pest test configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
 
 class User extends Authenticatable
 {
@@ -44,5 +45,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Check if the user can access the Filament admin panel.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // Allow all users to access the admin panel for testing
     }
 }
